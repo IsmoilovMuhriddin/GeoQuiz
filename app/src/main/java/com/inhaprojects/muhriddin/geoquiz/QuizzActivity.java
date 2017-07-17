5cdc1b7e4165e503e562846c6457d18c35419ce4
Muhriddin <ismoilovmuh1996@gmail.com>
2017-07-17 16:29:44 +0900

    Example function listener, input output

--- app/src/main/java/com/inhaprojects/muhriddin/geoquiz/QuizzActivity.java
+++ app/src/main/java/com/inhaprojects/muhriddin/geoquiz/QuizzActivity.java
@@ -2,6 +2,9 @@ package com.inhaprojects.muhriddin.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class QuizzActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class QuizzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
+    }
+
+
+    public void GreetingMethod(View view) {
+        TextView output = (TextView) findViewById(R.id.printView);
+        EditText input =(EditText) findViewById(R.id.inputName);
+        output.setText(input.getText());
+
     }
 }
